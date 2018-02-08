27215fdd32beb4a12152f7582036bb28c322de26
Abhik <abhiksetia@gmail.com>
2018-02-08 20:26:10 +0530

    backend 70% done

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,57 @@
 $(document).ready(function($) {
+
 		 var all_inputs=$(":input");
 
 		 var user_name=all_inputs[1];
 		 var user_password=all_inputs[2];
-
+		 check_user_storage();
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    var message_details=request.message_details;
+
+    if (message_details.message == "save"){
+    	save_changes(message_details.username,message_details.password,sendResponse);
+    }
+  });
+
+function check_user_storage() {
+	 chrome.storage.sync.get("user_details", function(items) {
+
+	    if (!chrome.runtime.error) {
+			if(items.user_details!=null){
+				//user details present
+				message_details={
+					message:"hide",
+					username:items.user_details.username,
+					password:items.user_details.password
+				};
+					chrome.runtime.sendMessage({message_details:message_details}, function(response) {});
+					console.log('user details found');
+			}else{
+				message_details={
+					message:"show",
+					username:null,
+					password:null
+				};
+				console.log('user details empty');
+				chrome.runtime.sendMessage({message_details:message_details}, function(response) {});
+			}
+		}else{
+	      console.log('Chrome Runtime Error : '+chrome.runtime.error);
+	 	}
+		});
+	}
+
+function save_changes (username,password,sendResponse) {
+	 	var user_details={
+		 	username:username,
+		 	password:password
+		 }
+		 
+		 chrome.storage.sync.set({'user_details':user_details}, function() {
+		 	console.log('Saved details');
+	      });	
+	 sendResponse({message:"saved"});
+}
---
+++ js/popup.js
@@ -0,0 +1,49 @@
+$("#save_btn").click(function(event) {
+
+	user_name=$("#roll_no").val();
+	user_password=$("#password").val();
+
+	if(!user_name || !user_password){
+	  	Materialize.toast("Homie!! Fill full details :P ", 2000);
+	}else{
+
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	message_details={
+		message:"save",
+		username:user_name,
+		password:user_password
+	};
+
+	chrome.tabs.sendMessage(tabs[0].id, {message_details: message_details }, function(response) {
+	  	console.log(response);
+	    if(response.message=="Not saved"){
+	    	Materialize.toast("Homie!! Fill full details :P ", 2000);
+	    }else{
+	    	Materialize.toast("Let's Roll bro.. I saved everything! ", 2000);
+	    	$("#before_login").hide();
+	    	$("#username_display").html(message_details.username);
+	    	$("#password_display").html(message_details.password);
+	    	$("#after_login").show();  	 	        
+	   }
+	  	});
+
+	});
+	}
+}); 
+
+$(document).ready(function($) {
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    var message_details=request.message_details;
+    console.log('received'+message_details);
+    if (message_details.message == "show"){
+      	$("#after_login").hide();  	 	        
+	  	$("#before_login").show();   	
+    }else{
+ 		$("#before_login").hide();
+		$("#username_display").html(message_details.username);
+	   	$("#password_display").html(message_details.password);
+	   	$("#after_login").show();  
+    }
+  });	
+});
